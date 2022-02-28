4bccdc80ee0497f2df3d88402fe56813df12b9ef
hoangdtcg <hoang.du@codegym.vn>
2022-02-28 14:53:04 +0700

    Add middleware check register

---
+++ app/Http/Middleware/CheckRegister.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class CheckRegister
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->password !== $request->confirmPassword) {
+            Session::flash('message',"Password confirmation failed");
+            return redirect()->back();
+        }
+
+        return $next($request); //dua request den controller
+    }
+}
